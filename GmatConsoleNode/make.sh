3f0d3c288061fa0cbd8d8f949fa66ac6b92edecf
Matthew Balman <matthew.d.balman@nasa.gov>
2014-05-14 11:40:06 -0400

    Updates to make.sh script.

--- GmatConsoleNode/make.sh
+++ GmatConsoleNode/make.sh
@@ -4,4 +4,4 @@ node-gyp clean configure
 cp lib* build/
 node-gyp build
 cp build/Release/GmatConsoleNode.node node_modules/gmat-console-node/bin/
-cp build/lib*.so node_modules/gmat-console-node/lib/
+cp build/lib*.so node_modules/gmat-console-node/gmat
